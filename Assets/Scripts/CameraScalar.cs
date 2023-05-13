4c57eb02e6f7c70e093d27a6a6704dc3a8d2fb34
champa1n73 <giakhuong0703@gmail.com>
2023-05-13 17:20:20 +0700

    Add CameraScalar.cs

---
+++ Assets/Scripts/CameraScalar.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScalar : MonoBehaviour
+{
+
+    private Board board;
+    public float cameraOffset;
+    public float aspectRatio = 0.625f;
+    public float padding = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        if (board != null)
+        {
+            RepositionCamera(board.width - 1, board.height - 1);
+        }
+    }
+
+    void RepositionCamera(float x, float y)
+    {
+        Vector3 tempPosition = new Vector3(x/2, y/2, cameraOffset);
+        transform.position = tempPosition;
+        if (board.width > board.height)
+        {
+            Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
+        }
+        else
+        {
+            Camera.main.orthographicSize = (board.height / 2 + padding);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
